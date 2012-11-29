4dc5ac0f74d5d812e64e602d57fb5420e95b3589
Student1 <student1@skillsmatter.com>
2012-11-29 15:27:40 +0000

    dir structure (stubs only so dirs appear)

---
+++ src/java/yahtzee/Stub.java
@@ -0,0 +1,11 @@
+package yahtzee;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Student
+ * Date: 29/11/12
+ * Time: 15:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Stub {
+}
