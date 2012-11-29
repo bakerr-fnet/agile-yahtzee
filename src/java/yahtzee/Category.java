412db43b128b6c3b2138705cc65c4fe59caebdd3
Student1 <student1@skillsmatter.com>
2012-11-29 15:49:11 +0000

    categories

---
+++ src/java/yahtzee/Category.java
@@ -0,0 +1,28 @@
+package yahtzee;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Student
+ * Date: 29/11/12
+ * Time: 15:38
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Category {
+
+    CHANCE,
+    YAHTZEE,
+    ONES,
+    TWOS,
+    THREES,
+    FOURS,
+    FIVES,
+    SIXES,
+    PAIR,
+    TWO_PAIRS,
+    THREE_OF_A_KIND,
+    FOUR_OF_A_KIND,
+    SMALL_STRAIGHT,
+    LARGE_STRAIGHT,
+    FULL_HOUSE
+
+}
