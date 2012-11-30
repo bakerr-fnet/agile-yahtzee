95c5c070b0b9629198fecf334766fe943dbfae47
agileA <agileA>
2012-11-30 13:53:00 +0000

    added pair, two pair, 3 of a kind, 4 of a kind + tests

--- src/java/yahtzee/Category.java
+++ src/java/yahtzee/Category.java
@@ -54,22 +54,62 @@ public enum Category {
     },
     PAIR {
         public int calculateScore(int... dice) {
-            return -1;
+
+            int[] res = getInstanceCount(dice);
+            for(int i = 5; i >=0; i--)
+            {
+                if(res[i]>1)
+                    return (i+1)*2;
+            }
+
+            return 0;
         }
     },
     TWO_PAIRS {
         public int calculateScore(int... dice) {
-            return -1;
+            int ret = 0;
+            int pairCount = 0;
+
+            int[] res = getInstanceCount(dice);
+            for(int i = 5; i >=0 && pairCount<2; i--)
+            {
+                if(res[i]>1)
+                {
+                    pairCount++;
+                    ret +=  (i+1)*2;
+                    res[i]-=2;
+                    i++;
+                }
+            }
+            if(pairCount==2)
+                return ret;
+            return 0;
         }
     },
-    THREE_OF_A_KIND {
+    THREE_OF_A_KIND{
         public int calculateScore(int... dice) {
-            return -1;
+
+            int[] res = getInstanceCount(dice);
+            for(int i = 5; i >=0; i--)
+            {
+                if(res[i]>2)
+                    return (i+1)*3;
+            }
+
+            return 0;
         }
     },
     FOUR_OF_A_KIND {
         public int calculateScore(int... dice) {
-            return -1;
+
+            int[] res = getInstanceCount(dice);
+            for(int i = 5; i >=0; i--)
+            {
+                if(res[i]>3)
+                    return (i+1)*4;
+            }
+
+            return 0;
         }
     },
     SMALL_STRAIGHT {
@@ -89,5 +129,15 @@ public enum Category {
     };
 
     abstract public int calculateScore(int... dice);
+
+    private static int[] getInstanceCount(int... dice){
+        int[] instanceCount = new int[] {0,0,0,0,0,0};
+
+        for(int c = 0; c < dice .length; c++)
+        {
+            instanceCount[dice[c]-1]++;
+        }
+        return instanceCount;
+    }
 
 }
--- src/test/yahtzee/CategoryTest.java
+++ src/test/yahtzee/CategoryTest.java
@@ -4,13 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Student
- * Date: 29/11/12
- * Time: 15:55
- * To change this template use File | Settings | File Templates.
- */
 public class CategoryTest {
 
     @Test
@@ -25,6 +18,41 @@ public class CategoryTest {
     public void score_for_chance_is_sum_of_all_dice() {
         Category ones = Category.CHANCE;
         assertEquals(17, ones.calculateScore(3,2,6,5,1));
+    }
+
+    @Test
+    public void score_for_pair_is_highest(){
+        Category pairs = Category.PAIR;
+        assertEquals(12,pairs.calculateScore(2,4,3,6,6));
+        assertEquals(12,pairs.calculateScore(2,3,3,6,6));
+        assertEquals(0,pairs.calculateScore(2,4,3,1,5));
+        assertEquals(12,pairs.calculateScore(2,4,6,6,6));
+    }
+
+    @Test
+    public void score_for_two_pair_is_added(){
+        Category twoPairs = Category.TWO_PAIRS;
+        assertEquals(0, twoPairs.calculateScore(2, 4, 3, 6, 6));
+        assertEquals(18, twoPairs.calculateScore(2, 3, 3, 6, 6));
+        assertEquals(0,twoPairs.calculateScore(2,4,3,1,5));
+        assertEquals(8, twoPairs.calculateScore(2, 2, 2, 2, 6));
+    }
+
+    @Test
+    public void score_for_three_of_a_kind_is_added(){
+        Category threeOfAKind = Category.THREE_OF_A_KIND;
+        assertEquals(18, threeOfAKind.calculateScore(2, 4, 6, 6, 6));
+        assertEquals(0, threeOfAKind.calculateScore(2, 3, 3, 6, 6));
+        assertEquals(18,threeOfAKind.calculateScore(6,6,6,6,6));
+    }
+
+    @Test
+    public void score_for_four_of_a_kind_is_added(){
+        Category fourOfAKind = Category.FOUR_OF_A_KIND;
+        assertEquals(4, fourOfAKind.calculateScore(1,1,1,1,5));
+        assertEquals(4, fourOfAKind.calculateScore(1,1,1,1,1));
+        assertEquals(4,fourOfAKind.calculateScore(1,1,4,1,1));
+        assertEquals(0, fourOfAKind.calculateScore(2, 2, 2, 6, 6));
     }
 
 }
